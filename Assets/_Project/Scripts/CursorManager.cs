565676d1cf9a0f17fd9bb16547ae9614f9478f42
ResNeedle <kemmachat.muangkaew@gmail.com>
2026-04-20 20:57:44 +0700

    [Add] group selection (unfinish "OS" version)
    commiting this just in case I need this later
    things that this version needed to be funcctional:
    - edge cases for deselection

--- Assets/_Project/Scripts/CursorManager.cs
+++ Assets/_Project/Scripts/CursorManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Unity.Mathematics;
 using UnityEditorInternal;
 using UnityEngine;
@@ -7,6 +8,7 @@ using UnityEngine.InputSystem;
 public class CursorManager : MonoBehaviour
 {
     Camera mainCam;
+    BoxCollider2D col2D;
 
     InputAction action_point;
     InputAction action_click;
@@ -16,10 +18,12 @@ public class CursorManager : MonoBehaviour
 
     Vector3 mouseClickPos;
     bool isDragging;
+    bool isGroupSelecting;
 
     void Awake()
     {
         mainCam = Camera.main;
+        col2D = GetComponent<BoxCollider2D>();
 
         action_point = InputSystem.actions.FindAction("Point");
         action_click = InputSystem.actions.FindAction("Click");
@@ -49,17 +53,29 @@ public class CursorManager : MonoBehaviour
         
         if (isDragging)
         {
-            inSelection[0]?.Drag(mouseScrPos, mouseVelo);
+            foreach (var ele in inSelection)
+            {
+                if (ele)
+                ele.Drag(mouseScrPos, mouseVelo);
+            }
+        }
+
+        if (isGroupSelecting)
+        {
+            UpdateBox(mouseClickPos, mouseScrPos);
         }
     }
 
     void Click(InputAction.CallbackContext ctx)
     {
         // clear selections
-        ClearSelection();
 
         var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         var found = Physics2D.OverlapCircle(mousePos, .1f);
+
+        mouseClickPos = mousePos;
+
+        //ClearConditionCheck();
 
         if (found)
         {
@@ -74,19 +90,30 @@ public class CursorManager : MonoBehaviour
                 AddSelection(scr_ele);
 
                 // store offset to each selected screen element
-                mouseClickPos = mainCam.ScreenToWorldPoint(Input.mousePosition);
-                scr_ele.UpdateOffset(mouseClickPos);
+                foreach (var ele in inSelection)
+                {
+                    ele.UpdateOffset(mouseClickPos);
+                }
+                
                 isDragging = true;
                 
             }
         }
         // else start box selection
+        else
+        {
+            print("Start Gruup Select");
+            isGroupSelecting = true;
+            EnableCollider(mouseClickPos);
+        }
     }
 
     void ClickRelease(InputAction.CallbackContext ctx)
     {
         print("release");
         isDragging = false;
+        isGroupSelecting = false;
+        DisableCollider();
     }
 
     void RightClick(InputAction.CallbackContext ctx)
@@ -97,17 +124,70 @@ public class CursorManager : MonoBehaviour
 
     void AddSelection(ScreenElements scr_ele)
     {
-        scr_ele.StateOverride(ScreenElements.ScreenElementState.Freeze);
-        inSelection.Add(scr_ele);
-    }
-
-    void ClearSelection()
-    {
-        foreach (var ele in inSelection)
+        if (!inSelection.Contains(scr_ele))
         {
-            ele.StateOverride(ScreenElements.ScreenElementState.Normal);
+            inSelection.Add(scr_ele);
+            scr_ele.StateOverride(ScreenElements.ScreenElementState.Freeze);
         }
-
-        inSelection.Clear();
     }
+
+    void RemoveSelection(ScreenElements scr_ele)
+    {
+        if (inSelection.Contains(scr_ele))
+        {
+            inSelection.Remove(scr_ele);
+            scr_ele.StateOverride(ScreenElements.ScreenElementState.Normal);
+        }
+    }
+
+    void ClearConditionCheck(ScreenElements scr_ele)
+    {
+        /// clear if :
+        /// 1. selecting file that are not being selected
+        /// 2. releasing click on blank space
+
+        
+    }
+
+    #region GroupSelectBox
+
+    public void EnableCollider(Vector2 mousePos)
+    {
+        transform.position = mousePos;
+    }
+
+    public void DisableCollider()
+    {
+        col2D.size = Vector2.zero;
+    }
+
+    public void UpdateBox(Vector2 startPoint, Vector2 endPoint)
+    {
+        var center = (startPoint + endPoint) / 2;
+        transform.position = center;
+        col2D.size = new Vector2(Mathf.Abs(endPoint.x - startPoint.x), Mathf.Abs(startPoint.y - endPoint.y));
+    }
+
+    
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.TryGetComponent<ScreenElements>(out var scr_ele);
+
+        if (scr_ele)
+        {
+            AddSelection(scr_ele);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        collision.TryGetComponent<ScreenElements>(out var scr_ele);
+
+        if(scr_ele && isGroupSelecting)
+        {
+            RemoveSelection(scr_ele);
+        }
+    }
+
+    #endregion
 }
