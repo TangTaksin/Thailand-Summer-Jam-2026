40e705a61e3a13717f1f0282ef3478728f50fc5b
Tang <taksin_tang51@hotmail.com>
2026-04-21 22:29:36 +0700

    [Add] firewall file

--- Assets/_Project/Scripts/ScreenMate/ScreenMateStats.cs
+++ Assets/_Project/Scripts/ScreenMate/ScreenMateStats.cs
@@ -15,22 +15,52 @@ public class ScreenMateStats : MonoBehaviour
 
     private bool _isGameOver = false;
 
+    public float _invincibleTimer = 0f;
+    public bool _isInvincible => _invincibleTimer > 0;
 
+    private SpriteRenderer spriteRenderer;
 
 
     private void Start()
     {
         if (_cortisolSlider) _cortisolSlider.maxValue = _maxCortisol;
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+
     }
 
     private void Update()
     {
+        if (_invincibleTimer > 0)
+        {
+            _invincibleTimer -= Time.deltaTime;
+        }
+
         UpdateCortisol(_passiveIncreaseRate * Time.deltaTime);
+
+        if (spriteRenderer != null)
+        {
+            if (_isInvincible)
+            {
+                spriteRenderer.color = Color.gold;
+            }
+            else
+            {
+                spriteRenderer.color = Color.white;
+            }
+            
+        }
     }
 
     public void UpdateCortisol(float amount)
     {
         if (_isGameOver) return;
+
+        if (_isInvincible && amount > 0)
+        {
+            return;
+        }
 
         CurrentCortisol = Mathf.Clamp(CurrentCortisol + amount, 0, _maxCortisol);
 
@@ -44,12 +74,17 @@ public class ScreenMateStats : MonoBehaviour
         }
     }
 
+    public void ActivateInvincibility(float duration)
+    {
+        _invincibleTimer = duration;
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         IStatModifier effect = other.GetComponent<IStatModifier>();
         if (effect != null)
         {
-            effect.ApplyModifier(this);
+            effect.ApplyModifier(this);            
         }
     }
 
@@ -57,6 +92,7 @@ public class ScreenMateStats : MonoBehaviour
     {
         _isGameOver = false;
         CurrentCortisol = 0f;
+        _invincibleTimer = 0f;
 
         if (_cortisolSlider != null)
             _cortisolSlider.value = 0f;
